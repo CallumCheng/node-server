bc1fc76dc2ae517872ffa82140f0c3a47bd2505d
CallumCheng <callumcheng@outlook.com>
2022-07-14 13:04:34 +0100

    add page links and texts

--- server.js
+++ server.js
@@ -3,10 +3,30 @@ const http = require("http");
 
 const server = http.createServer((req,res)=> {
     // server logic 
+    console.log(req.url)
+
+    let body, code;
+    switch (req.url){
+        case "/":
+            body = "Welcome to the crazy cat corner";
+            code = 200;
+            break;
+        case "/cats":
+            body = "My cat's name is cleo"
+            code = 200
+            break;
+        default:
+            body = `${req.url} does not exist`
+            code = 404
+    }
+
+    res.statusCode = 200
+    res.end(body)
+
+    
 });
 
-server.listen(80, ()=> {            //callback function
+server.listen(80, ()=> {            //running the server on localhost 80
     console.log('Server running on port 80')
 });
 
-console.log('hello there')
